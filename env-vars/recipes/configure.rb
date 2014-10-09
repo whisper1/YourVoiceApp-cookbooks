94903ee6fab983a80f650e2f0450110cbf2b4108
Tom Glaz <tgmessages@gmail.com>
2014-10-09 15:29:54 -0700

    update configure.rb

--- env-vars/recipes/configure.rb
+++ env-vars/recipes/configure.rb
@@ -1,6 +1,6 @@
 if node[:opsworks][:instance][:layers].include?('rails-app')
 
-  include_recipe "opsworks_custom_env::restart_command"
-  include_recipe "opsworks_custom_env::write_config"
+  include_recipe "env-vars::restart_command"
+  include_recipe "env-vars::write_config"
 
 end
